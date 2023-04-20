3660ebd51ca971aaf0d25098111160640a1caa16
je-delray <hovax74935@ekcsoft.com>
2023-04-20 09:28:07 +0200

    third

--- src/components/model/list.ts
+++
@@ -1,1 +0,0 @@
-export const numbers = [1, 2, 3, 4, 5, 1, 2, 3, 4, 5]
--- src/components/ui/Numbers.tsx
+++ src/components/ui/Numbers.tsx
@@ -1,15 +1,28 @@
+import { useEffect, useState } from "react"
 import styles from "./Numbers.module.scss"
-import { numbers } from "../model/list"
+
+interface numbersProps {
+    numbers: any
+    length: any
+}
+
+const [isActive, setIsActive] = useState(numbers ? numbers.styles.backgroundColor="black")
+const activated = useEffect(() => {})
 
 function Numbers() {
     return (
         <div className={styles.wrapper}>
             <div className={styles.container}>
                 <div className={styles.numbers}>
-                    {numbers.map(number => (
-                        <div className={styles.number}>{number}</div>
-                    ))}
+                    {Array(10)
+                        .fill(Math.floor(Math.random() * 6))
+                        .map(number => (
+                            <div className={styles.number}>
+                                {Math.floor((Math.random() + 1) * number)}
+                            </div>
+                        ))}
                 </div>
+                <button className={styles.button}>Roll</button>
             </div>
         </div>
     )
